0ac0032376cfb605c3d5f7b8e199809ac0bb4ceb
Lenovo <Lenovo@DESKTOP-TID21I7>
2021-02-25 17:22:46 +0200

    Second commit

---
+++ CTS/src/clase/Zoo.java
@@ -0,0 +1,28 @@
+package clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+	private Zookeeper ingrijitor;
+	private List<Animal> animale;
+	public Zoo(Zookeeper ingrijitor, List<Animal> animale) {
+		super();
+		this.ingrijitor = ingrijitor;
+		this.animale = animale;
+	}
+	
+	public Zoo() {
+		ingrijitor = new Zookeeper("Gigel");
+		this.animale = new ArrayList<Animal>();
+	}
+	
+	public void addAnimal(Animal animal) {
+		animale.add(animal);
+	}
+    public void feedAllAnimals() {
+    	for(Animal animal:animale) {
+    		ingrijitor.feed(animal);
+    	}
+    }
+}
--- CTS/src/program/Main.java
+++ CTS/src/program/Main.java
@@ -1,10 +1,19 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("main");
-        
+		Zoo z = new Zoo();
+        Giraffe giraffe = new Giraffe("girafa");
+        Giraffe giraffe2 = new Giraffe("girafa 2");
+//        
+        z.addAnimal(giraffe);
+        z.addAnimal(giraffe2);
+        z.feedAllAnimals();
+		System.out.println("asa");
 	}
 
 }
